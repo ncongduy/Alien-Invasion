f6604090f723ef703cdcfd9ae0affa8547f9cabc
Nguyen Cong Duy <ncongduy@gmail.com>
2021-01-08 15:23:34 +0200

    finnish chapter 13, 14

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,9 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -11,13 +14,24 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 	
-	#Make a ship.
-	ship = Ship(screen)
+	# Create an instance to store game statistics.
+	stats = GameStats(ai_settings)
+	
+	# Make a ship, a group of bullets, and a group of aliens.
+	ship = Ship(ai_settings, screen)
+	bullets = Group()
+	aliens = Group()
+	
+	# Create the fleet of aliens.
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 	
 	#Start the main loop for the game.
 	while True:
-		gf.check_events(ship)
-		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 		
 run_game()
